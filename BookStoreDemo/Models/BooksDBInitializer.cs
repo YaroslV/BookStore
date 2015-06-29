df76b1b2bba080a78811f8cd486cf9d96845f088
YarosIV <yaroslav.voznij@gmail.com>
2015-06-29 14:37:20 +0300

    Work's started

--- BookStoreDemo/Models/BooksDBInitializer.cs
+++ BookStoreDemo/Models/BooksDBInitializer.cs
@@ -6,17 +6,16 @@ using System.Data.Entity;
 
 namespace BookStoreDemo.Models
 {
-    public class BooksDBInitializer : CreateDatabaseIfNotExists<BookStoreDemoDBContext>
+    public class BooksDBInitializer : DropCreateDatabaseAlways<BookStoreDemoDBContext>
     {
         protected override void Seed(BookStoreDemoDBContext context)
-        {
-
-            context.Books.Add(new Book() { BookName = "Fiesta", BookNumberOfPages = 453, BookAuthor = new Author() { AuthorName = "Hemingway"} });
-            context.Books.Add(new Book() { BookName = "Ulyssess", BookNumberOfPages = 990, BookAuthor = new Author() { AuthorName = "Joyce"} });
-            context.Books.Add(new Book() { BookName = "Plague", BookNumberOfPages = 232, BookAuthor = new Author() { AuthorName = "Camus"} });
-            context.Books.Add(new Book() { BookName = "Old and Sea", BookNumberOfPages = 123, BookAuthor = new Author() { AuthorName = "Hemingway"} });
-
-
+        {            
+            var Hemingway = new Author() { AuthorName = "Hemingway"} ;
+            context.Books.Add(new Book() { BookName = "Fiesta", Price = 453, BookAuthor = Hemingway});
+            context.Books.Add(new Book() { BookName = "Ulyssess", Price = 990, BookAuthor = new Author() { AuthorName = "Joyce" } });
+            context.Books.Add(new Book() { BookName = "Plague", Price = 232, BookAuthor = new Author() { AuthorName = "Camus" } });
+            context.Books.Add(new Book() { BookName = "Old and Sea", Price = 123, BookAuthor = Hemingway });
+            
             base.Seed(context);
         }
     }
